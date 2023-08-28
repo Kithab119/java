3fe6cb2ce8f30bb3d3291e4002cb20ba7eb25a24
EC2 Default User <ec2-user@ip-172-31-37-250.ap-northeast-1.compute.internal>
2023-08-28 15:37:51 +0000

    [aho]

--- practice.java
+++ practice.java
@@ -2,5 +2,8 @@ class practice {
   public static void main(String[] args) {
     System.out.println("Hello World");
     System.out.println("Hello World2");
+    System.out.println(100 + 30);
+    String greeting = "aho";
+    System.out.println(greeting);
   }
 }
