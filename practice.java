76a3476372039d64be1c58c481507d3c68a5043f
EC2 Default User <ec2-user@ip-172-31-37-250.ap-northeast-1.compute.internal>
2023-08-29 14:47:57 +0000

    [import scan]

--- practice.java
+++ practice.java
@@ -1,9 +1,16 @@
-class practice {
+import java.util.*;
+
+public class practice {
   public static void main(String[] args) {
     System.out.println("Hello World");
     System.out.println("Hello World2");
     System.out.println(100 + 30);
     String greeting = "aho";
     System.out.println(greeting);
+    Scanner scan = new Scanner(System.in);
+    String message = scan.next();
+    System.out.println("oh " + message);
+    int number = scan.nextInt();
+    System.out.println(number);
   }
 }
