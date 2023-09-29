a71975103a6dce03ca9cb0fb0abe24066c70a7f4
EC2 Default User <ec2-user@ip-172-31-37-250.ap-northeast-1.compute.internal>
2023-09-29 13:58:44 +0000

    [Javadocコメント]

--- practice.java
+++ practice.java
@@ -172,12 +172,33 @@ class method {
     return index;
   }
 }
-
+/**
+ * ユーティリティクラス
+ * <p>
+ * 便利なメソッドを集めたクラスです。<br>
+ * これからも追加予定
+ *
+ * @version 1.0
+ * @author Aさん
+ */
 class Util {
+  /**
+   * 四角形の面積を計算
+   *
+   * @param height 高さ
+   * @param width　幅
+   * @return 　　　四角形の面積
+   */
   public static double menseki(double height, double width) {
     double s = height * width;
     return s;
   }
+  /**
+   * 円の面積を計算する
+   *
+   * @param hankei 円の半径
+   * @return       円の面積
+   */
   public static double menseki(double hankei) {
     double s = Math.pow(hankei, 2) * Math.PI;
     return s;
