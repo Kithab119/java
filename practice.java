c43fa8a93285ba98bb97d70d8c4d81aca304dc83
EC2 Default User <ec2-user@ip-172-31-37-250.ap-northeast-1.compute.internal>
2023-09-30 12:16:33 +0000

    [switch文]

--- practice.java
+++ practice.java
@@ -237,16 +237,29 @@ class zyouken {
       System.out.println("うるう年ではありません");
     }
     int m = scan.nextInt();
-    if(m==12 || m>=1 && m<=2) {
-      System.out.println("冬");
-    }else if(m>=3 && m<=5) {
-      System.out.println("春");
-    }else if(m>=6 && m<=8) {
-      System.out.println("夏");
-    }else if(m>=9 && m<=11) {
-      System.out.println("秋");
-    }else if(m==0) {
-      System.out.println("月の値が不正です");
+    switch(m) {
+      case 12:
+      case 1:
+      case 2:
+        System.out.println("冬");
+        break;
+      case 3:
+      case 4:
+      case 5:
+        System.out.println("春");
+        break;
+      case 6:
+      case 7:
+      case 8:
+        System.out.println("夏");
+        break;
+      case 9:
+      case 10:
+      case 11:
+        System.out.println("秋");
+        break;
+      default:
+        System.out.println("月の値が不正です");
     }
   }
 }
