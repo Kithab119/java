625d194fbd21f4d4f39e125beb73dd9603353bde
Kithab119 <yuui.furugaki@gmail.com>
2023-10-12 13:11:01 +0000

    抽象クラス

--- practice.java
+++ practice.java
@@ -496,3 +496,25 @@ class gm {
     //あいうえ
   }
 }
+
+abstract class Member {
+  private long id;
+  private String name;
+  protected Member(long id, String name) {
+    this.id = id;
+    this.name = name;
+  }
+  protected abstract int kai_hi();
+  public long getId() {
+    return id;
+  }
+  public String getName() {
+    return name;
+  }
+  public void setId(long id) {
+    this.id = id;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+}
